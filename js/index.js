877fb2aae0d59a70cb0cde5f7418e034321d62f3
Patrick Dutra <patrickdutra100@hotmail.com>
2022-06-22 18:46:47 -0300

    Correção create-form

--- js/index.js
+++ js/index.js
@@ -38,20 +38,20 @@ document.getElementById("create-form").addEventListener("submit", function(e) {
     const email = document.getElementById("email-create-input").value;
     const password = document.getElementById("password-create-input").value;
 
-    if (email.length < 5) {
-        alert("Preencha o campo com um email válido.")
+    if(email.length < 5) {
+        alert("Preencha o campo com um email válido.");
         return;
     }
 
     if(password.length < 4) {
-        alert("Preencha a senha com no mínimo 4 dígitos.")
+        alert("Preencha a senha com no mínimo 4 dígitos.");
         return; 
     }     
 
     saveAccount({
         login: email,
         password: password,
-        transactions: [],
+        transactions: []
     });
 
     myModal.hide();
